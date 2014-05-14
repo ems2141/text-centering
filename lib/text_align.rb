1296f06236f65e4a047bf7668760ee09f8fd8572
Ellie Schneiders <ems2141@gmail.com>
2014-05-14 09:56:47 -0600

    updated align method.  still WIP

--- lib/text_align.rb
+++ lib/text_align.rb
@@ -2,12 +2,20 @@ require 'json'
 
 class TextAlign
   def initialize(text)
-    @text = JSON.parse(text) #error unexpected token
+    @text = text.split("\n")
   end
 
   def align
-    # read each line of the text submitted
-    # catch the longest line
+    line_1_length = @text[0].length
+    longest_line = line_1_length
+    @text.each do |line|
+      if line.length > line_1_length
+        longest_line = line
+      end
+    end
+
+    @text.each do |align|
+    longest_line
     # adjust length of shorter lines to equal the length of the longest line by adding spaces at the beginning and end
     # if there are an odd number of spaces to add, add an extra one to the end of the strings
 
